4a927dca701790424aee93beb7e06cfa691ecc14
RUAN VICTOR D ELIA <ruan.vdelia@senacsp.edu.br>
2026-02-03 11:47:54 -0300

    Criando fuções de CRUD

--- tarefas/api.php
+++ tarefas/api.php
@@ -35,11 +35,83 @@ try {
             "ok" => true,
             "tarefas" => $tarefas
         ]);
+
+        //Encerra 
+        exit;
     }
-    else{
+    
+
+    if($acao === "criar"){
+        $titulo = trim($_POST['titulo'] ?? "");
+    }
+    if($titulo === ""){
+        echo json_encode([
+            "ok" => false,
+            "msg" => "Titulo é obrigatório"
+        ]);
+        exit;
+        }
+    
+    //
+    $sql = "INSERT INTO tarefas (titulo) VALUE (?)";
+
+    $stmt->$conn->prepare($sql);
+    
+    $stmt->execute([$titulo]);
+
+    echo json_encode([
+        "ok" => true,
+        "msg" => 'Tarefa Criada'
+    ]);
+    exit;
+
+    if($acao === "atualizar"){
+        $id = $_POST['id'] ?? null;
+        $titulo = trim($_POST['titulo'] ?? "");
+
+        if(!$id || $titulo === ""){
+            echo json_encode([
+                "ok" => false,
+                "msg" => "Dados inválidos"
+            ]);
+            exit;
+        };
+    }
+
+    $sql = "UPDATE tarefas SET titulo = ? WHERE id = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([$titulo, $id]);
+
+    echo json_encode([
+        "ok" => true,
+        "msg" => "Tarefa atualizada"
+    ]);
+
+    if ($acao === "excluir"){
+        
+        $id = $_POST['id'] ?? null;
+
+        if(!$id){
+            echo json_encode([
+                "ok" => false,
+                "msg" => "Dados inválidos"
+            ]);
+        };
+
+        $sql = "DELETE tarefas = WHERE id = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute([$id]);
+
+        echo json_encode([
+            "ok" => true,
+            "msg" => "Tarefa excluída"
+        ]);
 
     }
-} catch (\Throwable $th) {
+
+
+
+    }catch (\Throwable $th) {
 
 }
 
